aebf90ad8d7b2ec1b19f62f439f8154c7bcf3fa6
chiliwis <cristinasanchezg@gmail.com>
2022-08-22 22:13:36 -0500

    arrray barriles

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,10 +1,14 @@
 console.log("Entro al main.js");
 
+let numbarril = [];
+
 const CrearBarril = document.getElementById ("CrearBarril");
 const BorrarBarril = document.getElementById ("BorrarBarril");
 
 function agregarBarril() {
-    console.log("Entro a agregarBarril");
+    const barril = CrearBarril.value;
+    numbarril.push(barril);
+    console.log(numbarril)
 }
 
 function borrarBarril() {
