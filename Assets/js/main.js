c677a893114b3d9d2298c4ffe6533aff1cdb3714
chiliwis <cristinasanchezg@gmail.com>
2022-08-22 22:01:19 -0500

    Agregar y Borrar Barril

---
+++ Assets/js/main.js
@@ -0,0 +1,12 @@
+console.log("Entro al main.js");
+
+const CrearBarril = document.getElementById ("CrearBarril");
+const BorrarBarril = document.getElementById ("BorrarBarril");
+
+function agregarBarril() {
+    console.log("Entro a agregarBarril");
+}
+
+function borrarBarril() {
+    console.log("Entro a borrarBarril");
+}
