796eb56b8be25e57b66d8cb1904ba3fff699c757
chiliwis <cristinasanchezg@gmail.com>
2022-08-25 15:37:00 -0500

    JSON LISTA

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,7 +1,7 @@
 console.log("Entro al main.js");
 
-let numbarril = [];
-let nombre_previo = "";
+let numbarriles = [];
+let barril_previo = "";
 let editando = false;
 
 const CrearBarril = document.getElementById ("CrearBarril");
@@ -9,18 +9,24 @@ const BorrarBarril = document.getElementById ("BorrarBarril");
 
 function agregarBarril() {
     const barril = CrearBarril.value;
-    numbarril.push(barril);
-    localStorage.setItem("numbarril", JSON.stringify(numbarril));
+    numbarriles.push(barril);
+    localStorage.setItem("numbarriles", JSON.stringify(numbarriles));
+    console.log(numbarriles);
 
     // console.log(numbarril)
 }
 
-function actualizarLista(){
+function actualizarbarriles (){
     
+} 
+
+function limpiarStorage(){
+    localStorage.clear();
+    numbarriles =[]; 
 }
 
-function borrarBarril() {
-   localStorage.clear();
-   numbarril = []
-   console.log(numbarril);
-}
+// function borrarBarril() {
+//    localStorage.clear();
+//    numbarril = []
+//    console.log(numbarril);
+// }
